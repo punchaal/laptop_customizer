7b725af5a3f07d8ba8b9ddc793b926248f06609a
Pratik <punchaal@gmail.com>
2019-04-12 08:11:09 -0400

    Updates

---
+++ precache-manifest.40ea27e70947de9d4d0e144393295416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86fad1fc3e2d20965f93",
+    "url": "/laptop_customizer/static/js/runtime~main.86fad1fc.js"
+  },
+  {
+    "revision": "5f0a3978da96dbc9239d",
+    "url": "/laptop_customizer/static/js/main.5f0a3978.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/laptop_customizer/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "5f0a3978da96dbc9239d",
+    "url": "/laptop_customizer/static/css/main.523be85f.chunk.css"
+  },
+  {
+    "revision": "3a0135353c75e2451fc381af30f23f9a",
+    "url": "/laptop_customizer/index.html"
+  }
+];
